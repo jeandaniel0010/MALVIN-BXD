29c78b9ff3924e054a463109cf97ea9758654347
jeandaniel0010 <jeandaniel0010@gmail.com>
2025-09-08 00:04:02 +0000

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,8 +1,8 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
+  SESSION_ID: "malvin~IqVwGKSZ#nZPBlKpDM10VVyuqmRTkvVJJUYNN-JCYWu9r8vvzKpU",  // add your Session Id here
   
-  OWNER_NUMBER: "263714757857", // put your phone number here
+  OWNER_NUMBER: "+2250152548568", // put your phone number here
   
   PREFIX: ".", // prefix (e.g., ., /, !, *)
   
